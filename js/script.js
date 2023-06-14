e64b17a4f9e6fe7da8c18fd09c250c9b879010e2
Bunyod1771 <dushanovbunyod@gmail.com>
2023-06-14 16:13:16 +0500

    project initialized

---
+++ js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+ // alert(" hello Javascript");
+
+
+ // const place = confirm( "are you here") ;
+
+// console.log(plase);
+
+
+const currentPlase  = prompt( "where are you", " Toshkent");
+console.log(currentPlase);
